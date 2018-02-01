7c29abb3c00955bcd918b6a8d75aa423c18ac357
ShiZhan <g.shizhan.g@gmail.com>
2018-02-01 10:00:06 +0800

    evaluate script

---
+++ cache-line/evaluate-cache.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+printf "\nL1_64\n"
+printf "step\thits\tmisses\taccesses\n"
+for i in {0..7}
+do
+  step=$[ 1<<$i ]
+  h=`grep "system.cpu.dcache.overall_hits::cpu.data" m5out-67108864-64-$step/stats.txt | awk '{print $2}'`
+  m=`grep "system.cpu.dcache.overall_misses::cpu.data" m5out-67108864-64-$step/stats.txt | awk '{print $2}'`
+  a=`grep "system.cpu.dcache.overall_accesses::cpu.data" m5out-67108864-64-$step/stats.txt | awk '{print $2}'`
+  printf "$step\t$h\t$m\t$a\n"
+done
+
+printf "\nL1_32\n"
+printf "step\thits\tmisses\taccesses\n"
+for i in {0..7}
+do
+  step=$[ 1<<$i ]
+  h=`grep "system.cpu.dcache.overall_hits::cpu.data" m5out-67108864-32-$step/stats.txt | awk '{print $2}'`
+  m=`grep "system.cpu.dcache.overall_misses::cpu.data" m5out-67108864-32-$step/stats.txt | awk '{print $2}'`
+  a=`grep "system.cpu.dcache.overall_accesses::cpu.data" m5out-67108864-32-$step/stats.txt | awk '{print $2}'`
+  printf "$step\t$h\t$m\t$a\n"
+done
+
+printf "\nL1_16\n"
+printf "step\thits\tmisses\taccesses\n"
+for i in {0..7}
+do
+  step=$[ 1<<$i ]
+  h=`grep "system.cpu.dcache.overall_hits::cpu.data" m5out-67108864-16-$step/stats.txt | awk '{print $2}'`
+  m=`grep "system.cpu.dcache.overall_misses::cpu.data" m5out-67108864-16-$step/stats.txt | awk '{print $2}'`
+  a=`grep "system.cpu.dcache.overall_accesses::cpu.data" m5out-67108864-16-$step/stats.txt | awk '{print $2}'`
+  printf "$step\t$h\t$m\t$a\n"
+done
+
