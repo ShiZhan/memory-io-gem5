7255beda765eaba36e1c81c2848f0dd9d6b29749
ShiZhan <g.shizhan.g@gmail.com>
2018-02-01 10:57:01 +0800

    inspect l2

--- cache-line/evaluate-cache.sh
+++ cache-line/evaluate-cache.sh
@@ -33,3 +33,35 @@ do
   printf "$step\t$h\t$m\t$a\n"
 done
 
+printf "\nL2_L1_64\n"
+printf "step\thits\tmisses\taccesses\n"
+for i in {0..7}
+do
+  step=$[ 1<<$i ]
+  h=`grep "system.l2.overall_hits::cpu.data" m5out-67108864-64-$step/stats.txt | awk '{print $2}'`
+  m=`grep "system.l2.overall_misses::cpu.data" m5out-67108864-64-$step/stats.txt | awk '{print $2}'`
+  a=`grep "system.l2.overall_accesses::cpu.data" m5out-67108864-64-$step/stats.txt | awk '{print $2}'`
+  printf "$step\t$h\t$m\t$a\n"
+done
+
+printf "\nL2_L1_32\n"
+printf "step\thits\tmisses\taccesses\n"
+for i in {0..7}
+do
+  step=$[ 1<<$i ]
+  h=`grep "system.l2.overall_hits::cpu.data" m5out-67108864-32-$step/stats.txt | awk '{print $2}'`
+  m=`grep "system.l2.overall_misses::cpu.data" m5out-67108864-32-$step/stats.txt | awk '{print $2}'`
+  a=`grep "system.l2.overall_accesses::cpu.data" m5out-67108864-32-$step/stats.txt | awk '{print $2}'`
+  printf "$step\t$h\t$m\t$a\n"
+done
+
+printf "\nL2_L1_16\n"
+printf "step\thits\tmisses\taccesses\n"
+for i in {0..7}
+do
+  step=$[ 1<<$i ]
+  h=`grep "system.l2.overall_hits::cpu.data" m5out-67108864-16-$step/stats.txt | awk '{print $2}'`
+  m=`grep "system.l2.overall_misses::cpu.data" m5out-67108864-16-$step/stats.txt | awk '{print $2}'`
+  a=`grep "system.l2.overall_accesses::cpu.data" m5out-67108864-16-$step/stats.txt | awk '{print $2}'`
+  printf "$step\t$h\t$m\t$a\n"
+done
