f4b78f5f4aff41a39ed9f0570516fcfeea0ac58a
ShiZhan <g.shizhan.g@gmail.com>
2018-02-02 12:15:50 +0800

    simplify

--- cache-line/evaluate-cache.sh
+++ cache-line/evaluate-cache.sh
@@ -1,67 +1,33 @@
 #!/bin/bash
 
-printf "\nL1_64\n"
-printf "step\thits\tmisses\taccesses\n"
-for i in {0..7}
-do
-  step=$[ 1<<$i ]
-  h=`grep "system.cpu.dcache.overall_hits::cpu.data" m5out-67108864-64-$step/stats.txt | awk '{print $2}'`
-  m=`grep "system.cpu.dcache.overall_misses::cpu.data" m5out-67108864-64-$step/stats.txt | awk '{print $2}'`
-  a=`grep "system.cpu.dcache.overall_accesses::cpu.data" m5out-67108864-64-$step/stats.txt | awk '{print $2}'`
-  printf "$step\t$h\t$m\t$a\n"
+for cache_line in 64 32 16; do
+  printf "\nL1_$cache_line\n"
+  printf "step\thits\tmisses\taccesses\n"
+  for i in {0..7}
+  do
+    step=$[ 1<<$i ]
+    h=`grep "system.cpu.dcache.overall_hits::cpu.data" m5out-67108864-$cache_line-$step/stats.txt | awk '{print $2}'`
+    m=`grep "system.cpu.dcache.overall_misses::cpu.data" m5out-67108864-$cache_line-$step/stats.txt | awk '{print $2}'`
+    a=`grep "system.cpu.dcache.overall_accesses::cpu.data" m5out-67108864-$cache_line-$step/stats.txt | awk '{print $2}'`
+    [ -z $h ] && h=0
+    [ -z $m ] && m=0
+    [ -z $a ] && m=0
+    printf "$step\t$h\t$m\t$a\n"
+  done
 done
 
-printf "\nL1_32\n"
-printf "step\thits\tmisses\taccesses\n"
-for i in {0..7}
-do
-  step=$[ 1<<$i ]
-  h=`grep "system.cpu.dcache.overall_hits::cpu.data" m5out-67108864-32-$step/stats.txt | awk '{print $2}'`
-  m=`grep "system.cpu.dcache.overall_misses::cpu.data" m5out-67108864-32-$step/stats.txt | awk '{print $2}'`
-  a=`grep "system.cpu.dcache.overall_accesses::cpu.data" m5out-67108864-32-$step/stats.txt | awk '{print $2}'`
-  printf "$step\t$h\t$m\t$a\n"
-done
-
-printf "\nL1_16\n"
-printf "step\thits\tmisses\taccesses\n"
-for i in {0..7}
-do
-  step=$[ 1<<$i ]
-  h=`grep "system.cpu.dcache.overall_hits::cpu.data" m5out-67108864-16-$step/stats.txt | awk '{print $2}'`
-  m=`grep "system.cpu.dcache.overall_misses::cpu.data" m5out-67108864-16-$step/stats.txt | awk '{print $2}'`
-  a=`grep "system.cpu.dcache.overall_accesses::cpu.data" m5out-67108864-16-$step/stats.txt | awk '{print $2}'`
-  printf "$step\t$h\t$m\t$a\n"
-done
-
-printf "\nL2_L1_64\n"
-printf "step\thits\tmisses\taccesses\n"
-for i in {0..7}
-do
-  step=$[ 1<<$i ]
-  h=`grep "system.l2.overall_hits::cpu.data" m5out-67108864-64-$step/stats.txt | awk '{print $2}'`
-  m=`grep "system.l2.overall_misses::cpu.data" m5out-67108864-64-$step/stats.txt | awk '{print $2}'`
-  a=`grep "system.l2.overall_accesses::cpu.data" m5out-67108864-64-$step/stats.txt | awk '{print $2}'`
-  printf "$step\t$h\t$m\t$a\n"
-done
-
-printf "\nL2_L1_32\n"
-printf "step\thits\tmisses\taccesses\n"
-for i in {0..7}
-do
-  step=$[ 1<<$i ]
-  h=`grep "system.l2.overall_hits::cpu.data" m5out-67108864-32-$step/stats.txt | awk '{print $2}'`
-  m=`grep "system.l2.overall_misses::cpu.data" m5out-67108864-32-$step/stats.txt | awk '{print $2}'`
-  a=`grep "system.l2.overall_accesses::cpu.data" m5out-67108864-32-$step/stats.txt | awk '{print $2}'`
-  printf "$step\t$h\t$m\t$a\n"
-done
-
-printf "\nL2_L1_16\n"
-printf "step\thits\tmisses\taccesses\n"
-for i in {0..7}
-do
-  step=$[ 1<<$i ]
-  h=`grep "system.l2.overall_hits::cpu.data" m5out-67108864-16-$step/stats.txt | awk '{print $2}'`
-  m=`grep "system.l2.overall_misses::cpu.data" m5out-67108864-16-$step/stats.txt | awk '{print $2}'`
-  a=`grep "system.l2.overall_accesses::cpu.data" m5out-67108864-16-$step/stats.txt | awk '{print $2}'`
-  printf "$step\t$h\t$m\t$a\n"
+for cache_line in 64 32 16; do
+  printf "\nL2_L1_$cache_line\n"
+  printf "step\thits\tmisses\taccesses\n"
+  for i in {0..7}
+  do
+    step=$[ 1<<$i ]
+    h=`grep "system.l2.overall_hits::cpu.data" m5out-67108864-$cache_line-$step/stats.txt | awk '{print $2}'`
+    m=`grep "system.l2.overall_misses::cpu.data" m5out-67108864-$cache_line-$step/stats.txt | awk '{print $2}'`
+    a=`grep "system.l2.overall_accesses::cpu.data" m5out-67108864-$cache_line-$step/stats.txt | awk '{print $2}'`
+    [ -z $h ] && h=0
+    [ -z $m ] && m=0
+    [ -z $a ] && m=0
+    printf "$step\t$h\t$m\t$a\n"
+  done
 done
